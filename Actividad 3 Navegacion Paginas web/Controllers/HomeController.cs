35bdde6211866423d49c14223e776cf622ec5ecb
Nayla Alejandra Garcia Mireles <171G0227@rcarbonifera.tecnm.mx>
2020-09-18 17:28:00 -0500

    Agregar archivos de proyecto.

---
+++ Actividad 3 Navegacion Paginas web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Actividad_3_Navegacion_Paginas_web.Models;
+namespace Actividad_3_Navegacion_Paginas_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+             villancicosContext context= new villancicosContext();
+            var villancico = context.Villancico.OrderBy(x => x.Nombre);
+
+            return View(villancico);
+        }
+
+        public IActionResult Informacion(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            villancicosContext context = new villancicosContext();
+            var vilancico = context.Villancico.FirstOrDefault(X => X.Id == id);
+
+            if (vilancico == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(vilancico);
+            }
+
+        }
+    }
+}
---
+++ Actividad 3 Navegacion Paginas web/Models/Villancico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad_3_Navegacion_Paginas_web.Models
+{
+    public partial class Villancico
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Compositor { get; set; }
+        public int? Anyo { get; set; }
+        public string Letra { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
