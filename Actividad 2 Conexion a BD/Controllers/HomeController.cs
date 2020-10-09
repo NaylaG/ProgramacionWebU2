9d18c0d8ceb17b3a0f1c8f44f2ecf2267c4fe1a9
Nayla Alejandra Garcia Mireles <171G0227@rcarbonifera.tecnm.mx>
2020-10-08 19:28:23 -0500

    Actividad 2 Agregada a este repositorio

---
+++ Actividad 2 Conexion a BD/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Actividad_2_Conexion_a_BD.Models;
+
+namespace Actividad_2_Conexion_a_BD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+
+            return View(estados);
+        }
+    }
+}
---
+++ Actividad 2 Conexion a BD/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad_2_Conexion_a_BD.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
